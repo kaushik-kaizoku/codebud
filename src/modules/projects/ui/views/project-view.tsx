9d21100d3d6ee3b79459f03c8c7b1f7594977b2b
KAUSHIK <kaushikhrushikesh514@gmail.com>
2025-10-29 11:39:17 +0530

    12: fragment view

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -7,6 +7,7 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/componen
 
 import { MessagesContainer } from "../components/messages-container";
 import { ProjectHeader } from "../components/project-header";
+import { FragmentWeb } from "../components/fragment-web";
 
 interface Props {
  projectId: string
@@ -35,7 +36,7 @@ export const ProjectView = ({ projectId }: Props) => {
           defaultSize={65}
           minSize={50}
         >
-          TODO: Preview
+          {!!activeFragment && <FragmentWeb data={activeFragment}/>}
         </ResizablePanel>
       </ResizablePanelGroup>
     </div>

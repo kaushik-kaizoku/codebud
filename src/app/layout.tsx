396055d5fb56d06821d0ccee57295a22c64022f5
KAUSHIK <kaushikhrushikesh514@gmail.com>
2025-10-29 10:28:13 +0530

    11: project header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "@/components/ui/sonner";
 
 import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 import { Geist, Geist_Mono } from "next/font/google";
 
 import "./globals.css";
@@ -28,12 +29,14 @@ export default function RootLayout({
 }>) {
   return (
     <TRPCReactProvider> 
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Toaster />
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Toaster />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
     </TRPCReactProvider>
